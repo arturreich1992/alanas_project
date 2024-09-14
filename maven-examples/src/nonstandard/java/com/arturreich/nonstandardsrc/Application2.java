f032c03888e4523c72c8b30683f28211f88af752
Artur Reich <arturreich1992@gmail.com>
2024-09-15 00:53:18 +0200

    intialer push in das repo

---
+++ maven-examples/src/nonstandard/java/com/arturreich/nonstandardsrc/Application2.java
@@ -0,0 +1,10 @@
+package com.arturreich.nonstandardsrc;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
